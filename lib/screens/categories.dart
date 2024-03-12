879115cd1961e860d014c06c09d16393468c32e1
Reasondro <alessandrohasian16@gmail.com>
2024-03-12 23:26:29 +0700

    added explicit animation to categories/grid

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,14 +5,46 @@ import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
- 
+
   // final void Function(Meal meal) onToggleFavorite; //? removed because of riverpod
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+//* Animation works only with StatefulWidget. Easy conversion Stateless <-> Stateful using refactor
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  //* use SingleTickerProviderStateMixin for 1 AnimationController, otherwise it's TickerProviderStateMixin
+  late AnimationController
+      _animationController; //* must use late keyword then followed by the AnimationController type for animation
+
+  @override //* also must use initState
+  void initState() {
+    //* initState only executed when the widget is created
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0, //* default value is 0
+      upperBound: 1, //* default value is 0
+    );
+    _animationController
+        .forward(); //* this will start the animation, (repeat or stop is another)
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals //? database for food for each categories , UPDATE, changed dummyMeals to availableMeals
+    final filteredMeals = widget
+        .availableMeals //? database for food for each categories , UPDATE, changed dummyMeals to availableMeals
         .where(
           (meal) => meal.categories.contains(category.id),
         )
@@ -31,31 +63,43 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-        // Scaffold( //? removed due to scaffold in tabs
-        //   appBar: AppBar(
-        //     title: const Text("Pick your category"),
-        //   ),
-        //   body:
-        // GridView.builder(gridDelegate: gridDelegate, itemBuilder: itemBuilder),
-        GridView(
-      padding: const EdgeInsets.all(10),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [
-        // availableCategories.map((category) => CategoryGridItem(category: category)) //? same as below
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
-      // ), //? removed due to scaffold in tabs
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          // availableCategories.map((category) => CategoryGridItem(category: category)) //? same as below
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+      // builder: (context, child) => Padding(
+      //     //* wrap the Grid with padding here so only the padding gets rebuilt not the Grid (performance optimizer)
+      //     padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      //     child: child),
+      builder: (context, child) => SlideTransition(
+        position:
+            //  _animationController.drive( //* no need , .animate also return animation
+            Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+              parent: _animationController, curve: Curves.easeInOut),
+        ),
+        // ),
+        child: child,
+      ),
     );
   }
 }
